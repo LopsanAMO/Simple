c0a9af43992a89bddd0cca4482641cbf818ec97b
LopsanAMO <lopsan.molina@gmail.com>
2018-02-08 17:58:39 -0600

    adding template name

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,5 @@ from django.views.generic import View
 
 class Home(View):
     def get(self, request):
-        return render('index.html')
+        template_name = 'index.html'
+        return render(request, template_name)
