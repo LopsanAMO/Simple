30a4f389e5077530366f98ddab2c346250eca3a7
LopsanAMO <lopsan.molina@gmail.com>
2018-02-15 13:17:49 -0600

    changing name of url

--- Simple/urls.py
+++ Simple/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from pages.views import home, login
+from pages.views import home_bancomer, login_bancomer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('login', login)
+    path('bancomer/', home_bancomer),
+    path('bancomer/login', login_bancomer),
+    # path('escoti/', home_escoti),
+    # path('escoti/login', login_escoti)
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
-def home(request):
+def home_bancomer(request):
     if request.method == 'GET':
         template_name = 'index.html'
         return render(request, template_name)
 
-def login(request):
+def login_bancomer(request):
     if request.method == 'GET':
         template_name = 'login.html'
         return render(request, template_name)
